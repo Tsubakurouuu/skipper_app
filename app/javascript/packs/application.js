29d1f54f0d9b6f01cd2def7f0a22b3c5e75b6eb7
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-03 11:02:22 +0000

    J-13

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
-import "skippr.min"
-import "script.js"
+// import "skippr.min"
+// import "script.js"
 
 Rails.start()
 Turbolinks.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,7 @@ environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-      "window.jQuery": "jquery"
+    jQuery: 'jquery/src/jquery'
   })
 )
 
